81211a8933c793559724dd6d13cafb786403fb43
Syed ZainUlAbideen <shahzain30196@gmail.com>
2022-04-18 21:22:37 +0500

    init

--- lib/code-pipeline-stack.ts
+++ lib/code-pipeline-stack.ts
@@ -1,16 +1,17 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class CodePipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CodePipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambdafn = new lambda.Function(this, 'lambdafnn', {
+      functionName: 'my-new-pipeline-yaml',
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromInline(`exports.handler=async()=>return 'Hello world'`),
+      handler: 'index.handler'
+    })
+    
   }
 }
